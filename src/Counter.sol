2974df3c32973ee0cec43a8cadfad6d8ec6e469d
Varun Verma <varunverma855@gmail.com>
2022-11-26 22:01:31 -0500

    uniswap dependency

--- src/Counter.sol
+++ src/Counter.sol
@@ -1,5 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+pragma solidity =0.7.6;
+
+import "v3-core/contracts/UniswapV3Pool.sol";
 
 contract Counter {
     uint256 public number;
