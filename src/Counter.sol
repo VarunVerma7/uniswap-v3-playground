dbcc39c68a5b5c60176278b7985375f9b576fd58
Varun Verma <varunverma855@gmail.com>
2022-11-27 02:19:00 -0500

    More updates

--- src/Counter.sol
+++ src/Counter.sol
@@ -1,8 +1,128 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity =0.7.6;
+pragma solidity =0.8.6;
 
-import "v3-core/contracts/UniswapV3Pool.sol";
 
+interface SwapRouter {
+    function WETH9() external view returns (address);
+
+    function exactInput(ISwapRouter.ExactOutputParams memory params)
+        external
+        payable
+        returns (uint256 amountOut);
+
+    function exactInputSingle(ISwapRouter.ExactOutputSingleParams memory params)
+        external
+        payable
+        returns (uint256 amountOut);
+
+    function exactOutput(ISwapRouter.ExactOutputParams memory params)
+        external
+        payable
+        returns (uint256 amountIn);
+
+    function exactOutputSingle(
+        ISwapRouter.ExactOutputSingleParams memory params
+    ) external payable returns (uint256 amountIn);
+
+    function factory() external view returns (address);
+
+    function multicall(bytes[] memory data)
+        external
+        payable
+        returns (bytes[] memory results);
+
+    function refundETH() external payable;
+
+    function selfPermit(
+        address token,
+        uint256 value,
+        uint256 deadline,
+        uint8 v,
+        bytes32 r,
+        bytes32 s
+    ) external payable;
+
+    function selfPermitAllowed(
+        address token,
+        uint256 nonce,
+        uint256 expiry,
+        uint8 v,
+        bytes32 r,
+        bytes32 s
+    ) external payable;
+
+    function selfPermitAllowedIfNecessary(
+        address token,
+        uint256 nonce,
+        uint256 expiry,
+        uint8 v,
+        bytes32 r,
+        bytes32 s
+    ) external payable;
+
+    function selfPermitIfNecessary(
+        address token,
+        uint256 value,
+        uint256 deadline,
+        uint8 v,
+        bytes32 r,
+        bytes32 s
+    ) external payable;
+
+    function sweepToken(
+        address token,
+        uint256 amountMinimum,
+        address recipient
+    ) external payable;
+
+    function sweepTokenWithFee(
+        address token,
+        uint256 amountMinimum,
+        address recipient,
+        uint256 feeBips,
+        address feeRecipient
+    ) external payable;
+
+    function uniswapV3SwapCallback(
+        int256 amount0Delta,
+        int256 amount1Delta,
+        bytes memory _data
+    ) external;
+
+    function unwrapWETH9(uint256 amountMinimum, address recipient)
+        external
+        payable;
+
+    function unwrapWETH9WithFee(
+        uint256 amountMinimum,
+        address recipient,
+        uint256 feeBips,
+        address feeRecipient
+    ) external payable;
+
+    receive() external payable;
+}
+
+interface ISwapRouter {
+    struct ExactOutputParams {
+        bytes path;
+        address recipient;
+        uint256 deadline;
+        uint256 amountOut;
+        uint256 amountInMaximum;
+    }
+
+    struct ExactOutputSingleParams {
+        address tokenIn;
+        address tokenOut;
+        uint24 fee;
+        address recipient;
+        uint256 deadline;
+        uint256 amountOut;
+        uint256 amountInMaximum;
+        uint160 sqrtPriceLimitX96;
+    }
+}
 contract Counter {
     uint256 public number;
 
--- test/SingleSwap.t.sol
+++ test/SingleSwap.t.sol
@@ -1,25 +1,24 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity =0.7.6;
+pragma solidity =0.8.6;
+pragma experimental ABIEncoderV2;
 
 import "forge-std/Test.sol";
-import { IUniswapV3Pool } from "@uniswap/v3-core/contracts/interfaces/IUniswapV3Pool.sol";
-import { IUniswapV3Factory } from "@uniswap/v3-core/contracts/interfaces/IUniswapV3Pool.sol";
-import { TransferHelper } from "@uniswap/v3-core/contracts/libraries/TransferHelper.sol";
-import { SwapRouter } from "@uniswap/v3-periphery/contracts/SwapRouter.sol";
+import "src/Counter.sol";
 contract CounterTest is Test {
-    IUniswapV3Pool public pool;
+    // IUniswapV3Pool public pool;
     SwapRouter public swapRouter;
     address public weth = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
     address public usdc = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
 
     function setUp() public {
+        vm.rollFork(16059272);
         // counter = new IUniswapV3Pool();
-        swapRouter = new swapRouter(0xe592427a0aece92de3edee1f18e0157c05861564);
+        swapRouter = SwapRouter(0xe592427a0aece92de3edee1f18e0157c05861564);
     }
 
     function getWeth() public {
         weth = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
-        IUniswapV3Pool pool = swapRouter.getPool(
+        swapRouter.getPool(
         weth,
         usdc,
         5
